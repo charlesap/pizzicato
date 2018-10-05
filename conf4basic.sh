615f148a8269aa9f9f841e8a649e7bd14ecf0e3f
Charles Perkins <chuck@kuracali.com>
2018-10-05 13:27:26 -0700

    populate 61 key overlay

--- conf4basic.sh
+++ conf4basic.sh
@@ -27,7 +27,7 @@ amixer cset numid=3 1
 if [ "$1" == "kbd" ]; then 
 
 
-cat << EOF >> /root/12matrix.dts
+cat << EOF >> /root/61matrix.dts
     /dts-v1/;
     /plugin/;
     / {
@@ -36,7 +36,7 @@ cat << EOF >> /root/12matrix.dts
            fragment@0 {
               target-path = "/";
               __overlay__ {
-                 keypad: MATRIX4x5 {
+                 keypad: MATRIX6x11 {
                     compatible = "gpio-matrix-keypad";
                     debounce-delay-ms = <10>;
                     col-scan-delay-us = <10>;
@@ -44,15 +44,25 @@ cat << EOF >> /root/12matrix.dts
 		       try to use GPIO only lines
                        to keep SPI and I2C usable
                     */
-                    row-gpios = <&gpio 16 0    // 1
-                                 &gpio 20 0    // 2
-                                 &gpio 21 0>;    // 3
+                    row-gpios = <&gpio 4 0    // 0
+                                 &gpio 17 0    // 1
+                                 &gpio 27 0    // 2
+                                 &gpio 22 0    // 3
+                                 &gpio 5 0     // 4
+                                 &gpio 6 0>;   // 5
                                  
 
-                    col-gpios = <&gpio 26 0    // 4
-                                 &gpio 19 0    // 5
-                                 &gpio 13 0    // 6
-                                 &gpio 6 0>;    // 7
+                    col-gpios = <&gpio 13 0    // 0
+                                 &gpio 19 0    // 1
+                                 &gpio 26 0    // 2
+                                 &gpio 21 0    // 3
+                                 &gpio 20 0    // 4
+                                 &gpio 16 0    // 5
+                                 &gpio 12 0    // 6
+                                 &gpio 25 0    // 7
+                                 &gpio 24 0    // 8
+                                 &gpio 23 0    // 9
+                                 &gpio 18 0>;    // 10
                                  
                     /*
                       Keycodes from /usr/include/linux/input-event-codes.h
@@ -66,24 +76,145 @@ cat << EOF >> /root/12matrix.dts
                                     0x01000052
                                     // col0 row2 KEY_RIGHT
                                     0x0200006a
-				    // col1 row0 KEY_KP7
-                                    0x00010047
-                                    // col1 row1 KEY_KP8
-                                    0x01010048
-                                    // col1 row2 KEY_KP9
-                                    0x02010049
-                                    // col2 row0 KEY_KP4
-                                    0x0002004b
-                                    // col2 row1 KEY_KP5
-                                    0x0102004c
-                                    // col2 row2 KEY_KP6
-                                    0x0202004d
-                                    // col3 row0 KEY_KP1
-                                    0x0003004f
-                                    // col3 row1 KEY_KP2
-                                    0x01030050
-                                    // col3 row2 KEY_KP3
-                                    0x02030051>;
+                                    // col0 row3 KEY_LEFT
+                                    0x00000069
+                                    // col0 row4 KEY_KP0
+                                    0x01000052
+                                    // col0 row5 KEY_RIGHT
+                                    0x0200006a
+
+
+                                    // col1 row0 KEY_LEFT
+                                    0x00000069
+                                    // col1 row1 KEY_KP0
+                                    0x01000052
+                                    // col1 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col1 row3 KEY_LEFT
+                                    0x00000069
+                                    // col1 row4 KEY_KP0
+                                    0x01000052
+                                    // col1 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col2 row0 KEY_LEFT
+                                    0x00000069
+                                    // col2 row1 KEY_KP0
+                                    0x01000052
+                                    // col2 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col2 row3 KEY_LEFT
+                                    0x00000069
+                                    // col2 row4 KEY_KP0
+                                    0x01000052
+                                    // col2 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col3 row0 KEY_LEFT
+                                    0x00000069
+                                    // col3 row1 KEY_KP0
+                                    0x01000052
+                                    // col3 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col3 row3 KEY_LEFT
+                                    0x00000069
+                                    // col3 row4 KEY_KP0
+                                    0x01000052
+                                    // col3 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col4 row0 KEY_LEFT
+                                    0x00000069
+                                    // col4 row1 KEY_KP0
+                                    0x01000052
+                                    // col4 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col4 row3 KEY_LEFT
+                                    0x00000069
+                                    // col4 row4 KEY_KP0
+                                    0x01000052
+                                    // col4 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col5 row0 KEY_LEFT
+                                    0x00000069
+                                    // col5 row1 KEY_KP0
+                                    0x01000052
+                                    // col5 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col5 row3 KEY_LEFT
+                                    0x00000069
+                                    // col5 row4 KEY_KP0
+                                    0x01000052
+                                    // col5 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col6 row0 KEY_LEFT
+                                    0x00000069
+                                    // col6 row1 KEY_KP0
+                                    0x01000052
+                                    // col6 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col6 row3 KEY_LEFT
+                                    0x00000069
+                                    // col6 row4 KEY_KP0
+                                    0x01000052
+                                    // col6 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col7 row0 KEY_LEFT
+                                    0x00000069
+                                    // col7 row1 KEY_KP0
+                                    0x01000052
+                                    // col7 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col7 row3 KEY_LEFT
+                                    0x00000069
+                                    // col7 row4 KEY_KP0
+                                    0x01000052
+                                    // col7 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col8 row0 KEY_LEFT
+                                    0x00000069
+                                    // col8 row1 KEY_KP0
+                                    0x01000052
+                                    // col8 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col8 row3 KEY_LEFT
+                                    0x00000069
+                                    // col8 row4 KEY_KP0
+                                    0x01000052
+                                    // col8 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col9 row0 KEY_LEFT
+                                    0x00000069
+                                    // col9 row1 KEY_KP0
+                                    0x01000052
+                                    // col9 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col9 row3 KEY_LEFT
+                                    0x00000069
+                                    // col9 row4 KEY_KP0
+                                    0x01000052
+                                    // col9 row5 KEY_RIGHT
+                                    0x0200006a
+
+                                    // col10 row0 KEY_LEFT
+                                    0x00000069
+                                    // col10 row1 KEY_KP0
+                                    0x01000052
+                                    // col10 row2 KEY_RIGHT
+                                    0x0200006a
+                                    // col10 row3 KEY_LEFT
+                                    0x00000069
+                                    // col10 row4 KEY_KP0
+                                    0x01000052
+                                    // col10 row5 KEY_RIGHT
+                                    0x0200006a>;
+
+
 
                  };
               };
